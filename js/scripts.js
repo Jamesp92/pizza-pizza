5964011402c774a7601867832273dbb2a7272b93
pj perry <pjperry28@gmail.com>
2022-06-17 10:54:48 -0700

    add else if

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,23 @@ function pizzaPizza() {
   this.size = size;
   this.toppings = toppings;
 };
+
+pizzaPizza.prototype.price = function () {
+ let price = 0;
+ if (this.size === "x-small"){
+  price = 4;
+ }
+ else if (this.size === "small"){
+  price = 6;
+ }
+ else if (this.size === "medium"){
+  price = 10;
+ }
+ else if (this.size === "large"){
+  price = 14;
+ }
+ else (this.size === "super duper extra big") 
+   price = 35;
+
+ 
+};
