20f1b4a0c0f1cd92fec36016950c7a1f0a52e084
pj perry <pjperry28@gmail.com>
2022-06-17 16:39:34 -0700

    cant get size pricing to work

--- js/scripts.js
+++ js/scripts.js
@@ -3,46 +3,43 @@
 function Pizza(size,toppings) {
   this.size = size;
   this.toppings = toppings;
+  
 };
 
-//  $(document) .ready (function() {
-  let toppingPrice = 0;
-  let toppingHolder = [];
-  let topping = '';
+
+let price = 0;
+
 Pizza.prototype.price = function () {
-  let price = 0;
-  if (this.size === "x-small - $4"){
-    price = 4;
+  
+
+  if (this.size === "x-small"){
+    price += 4;
   }
-  else if (this.size === "small -$6"){
-    price = 6;
+  else if (this.size === "small"){
+  price += 6;
   }
-  else if (this.size === "medium - 10"){
-    price = 10;
+  else if (this.size === "medium"){
+   price += 10;
   }
-  else if (this.size === "large - $14"){
-    price = 14;
+  else if (this.size === "large"){
+  price += 14;
   }
-  else if (this.size === "super duper extra big - $35"){
+  else if (this.size === "super duper extra big"){
     
-    price = 35;
+   price += 35;
   }
-  return  price;
+   
+  return price ;
 } 
+
 let newPizza = new Pizza ("medium");
 
-newPizza.price();
+
 console.log(newPizza)
- $("input:checkbox").click(function() {
-    
-  toppingHolder.push(parseInt($(this).val()))
-  topping = $(this).attr('id')
-  console.log(topping)
-  
-  topPrice = topHolder.reduce(function(accumulator, currentValue){
-    return accumulator + currentValue
-  })
-});
+console.log(price)
+
+
+
 
 
 
