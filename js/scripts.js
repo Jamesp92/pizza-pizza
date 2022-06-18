a8fa3850df30d1b737332b3933b1c71fd2ab5359
pj perry <pjperry28@gmail.com>
2022-06-18 12:06:06 -0700

    stuck on next step

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,45 @@
 // Bussiness logic
 
-function Pizza(size,toppings) {
+function Pizza(size,toppings,price) {
   this.size = size;
   this.toppings = toppings;
-  
+  this.priceArray =[];
 };
 
 
-let price = 0;
+
 
 Pizza.prototype.price = function () {
   
-
+  let price = 10
+  
   if (this.size === "x-small"){
-    price += 4;
+    price -= 6;
   }
   else if (this.size === "small"){
-  price += 6;
+ price -= 4;
   }
   else if (this.size === "medium"){
-   price += 10;
+    price = 10;
   }
   else if (this.size === "large"){
-  price += 14;
+    price += 4;
   }
   else if (this.size === "super duper extra big"){
     
-   price += 35;
+    price += 35;
   }
-   
-  return price ;
+  return price;
+  
 } 
 
-let newPizza = new Pizza ("medium");
+// price.push(this.priceArray) ;
+  
+let newPizza = new Pizza ();
 
-
-console.log(newPizza)
 console.log(price)
+console.log(newPizza)
+
 
 
 
