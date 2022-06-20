4a0db39401c54b9bf46ee039bec2f4366ff32c77
pj perry <pjperry28@gmail.com>
2022-06-20 12:02:41 -0700

    working on getting order to show on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-// Bussiness logic
+ 
+ // Business Logic for orderBook ---------
+function OrderBook (pizzas,currentId) {
+  this.pizzas = {};
+  this.currentId = 0;
+}
+ 
+OrderBook.prototype.addPizza = function(pizza) {
+  pizza.id = this.assignId();
+  this.pizzas[pizza.id] = pizza;
+};
+
+ OrderBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+
+// Bussiness logic for orders
 
 function Pizza(size,toppings,toppings2,pizzaPrice) {
   this.size = size;
@@ -9,14 +27,13 @@ function Pizza(size,toppings,toppings2,pizzaPrice) {
 
 
 
-
 Pizza.prototype.price = function () {
  
   if (this.size === "x-small"){
     this.pizzaPrice -= 6;
   }
   else if (this.size === "small"){
-  this.pizzaPrice -= 4;
+    this.pizzaPrice -= 4;
   }
   else if (this.size === "medium"){
     this.pizzaPrice = 10;
@@ -31,13 +48,14 @@ Pizza.prototype.price = function () {
   return this.pizzaPrice;
 }
 
+
 // Pizza.prototype.toppingPrice = function () {
- 
-// if (this.topping === "Mushrooms"){
-//   this.pizzaPrice +=1
-// }
-// else if (this.topping === "peperoni"){
-//   this.pizzaPrice +=1
+  
+  // if (this.topping === "Mushrooms"){
+    //   this.pizzaPrice +=1
+    // }
+    // else if (this.topping === "peperoni"){
+      //   this.pizzaPrice +=1
 // }
 // else if (this.topping === "Jalepenos"){
 //   this.pizzaPrice +=1
@@ -45,13 +63,14 @@ Pizza.prototype.price = function () {
 //   return this.pizzaPrice;
 // }
 
-  
 
 
 
 
-function displayPizzaOrder(displayPizzaOrder) {
-}
+
+// function displayPizzaOrder(displayPizzaOrder) {
+// }
+let newOrder = new OrderBook ();
 
 $(document).ready(function() {
   $("form#pizzaParlor").submit(function(event) {
@@ -60,10 +79,11 @@ $(document).ready(function() {
     let inputtedToppings = $("input:radio[name=tops]:checked").val();
     let inputtedToppings2 = $("input:radio[name=tops2]:checked").val();
     let finalPizza = new Pizza(inputtedSize, inputtedToppings,inputtedToppings2);
-   
-    finalPizza.price()
     
-    console.log(finalPizza);
+    // finalPizza.price()
+    OrderBook.addPizza(finalPizza);
+    console.log(OrderBook.contacts);
+    console.log(finalPizza,this.pizzas);
     
   });
 });
