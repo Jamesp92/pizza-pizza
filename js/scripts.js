2192bffe5949b3fa1a9b98e98dd6bdb536f61238
pj perry <pjperry28@gmail.com>
2022-06-19 21:40:27 -0700

    add ui logic for user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 // Bussiness logic
 
-function Pizza(size,toppings,pizzaPrice) {
+function Pizza(size,toppings,toppings2,pizzaPrice) {
   this.size = size;
   this.toppings = toppings;
+  this.toppings2 = toppings2
   this.pizzaPrice = 10 ;
 };
 
@@ -30,17 +31,19 @@ Pizza.prototype.price = function () {
   return this.pizzaPrice;
   
 } 
-let newPizza = new Pizza ("large");
 
-
-console.log(newPizza)
-
-
-
-
-
-
-
-
+function displayPizzaOrder(displayPizzaOrder) {
+}
+$(document).ready(function() {
+  $("form#pizzaParlor").submit(function(event) {
+    event.preventDefault();
+    let inputtedSize = $(".form-control#pizzaSlice").val();
+    let inputtedToppings = $("input:checkbox[name=tops]:checked").val();
+    let inputtedToppings2 = $("input:checkbox[name=tops2]:checked").val();
+    let finalPizza = new Pizza(inputtedSize, inputtedToppings,inputtedToppings2);
+   
+    finalPizza.price();
   
-
+    console.log(finalPizza);
+  });
+});
