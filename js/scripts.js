b388b2710ff22fcb3e6bb2db45c7fc1bd932292c
pj perry <pjperry28@gmail.com>
2022-06-25 15:21:32 -0700

    create array for toppings trying to find out how to add topping total to pizza total

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,59 @@
  
 // // Bussiness logic for orders
 
-function Pizza(size,toppings,toppings2,pizzaPrice) {
+function Pizza(size,toppings) {
   this.size = size;
-  this.toppings = toppings;
-  this.toppings2 = toppings2
-  this.pizzaPrice = 10 ;
-  this.toppingTotal = 0;
+  this.pizzaPrice = 10;
+
 };
 
 
+/// UI LOGIC
+        $(document).ready(function() {
+          $("form#pizzaParlor").submit(function(event) {
+            event.preventDefault();
+            Pizza.prototype.price = function () {
+             
+              if (this.size === "x-small"){
+                this.pizzaPrice -= 6;
+              }
+              else if (this.size === "small"){
+                this.pizzaPrice -= 4;
+              }
+              else if (this.size === "medium"){
+                this.pizzaPrice = 10;
+              }
+              else if (this.size === "large"){
+                this.pizzaPrice += 4;
+              }
+              else if (this.size === "super duper extra big"){
+                
+                this.pizzaPrice += 35;
+              } else{
+                this.pizzaPrice
+              }
+              return this.pizzaPrice;
+            }
+            
+            let inputtedSize = $(".form-control#pizzaSlice").val();
+            let inputtedToppings = $("input:radio[name=tops]:checked").val();
+            let inputtedToppings2 = $("input:radio[name=tops2]:checked").val();
+            let finalPizza = new Pizza(inputtedSize);
+            let finalPrice = this.toppingsPrice + this.pizzaPrice
 
-Pizza.prototype.price = function () {
- 
-  if (this.size === "x-small"){
-    this.pizzaPrice -= 6;
-  }
-  else if (this.size === "small"){
-    this.pizzaPrice -= 4;
-  }
-  else if (this.size === "medium"){
-    this.pizzaPrice = 10;
-  }
-  else if (this.size === "large"){
-    this.pizzaPrice += 4;
-  }
-  else if (this.size === "super duper extra big"){
+            this.toppingsPrice = 0;
+            this.toppingArray = [inputtedToppings,inputtedToppings2];
+            this.toppingArray.forEach(() => {
+              this.toppingsPrice += 2;
+              return this.toppingsPrice 
+            });
+            $("#order").text("Your , " + " " + inputtedSize + " , "  +"pizza" + " with  , " + inputtedToppings + " , and , " + inputtedToppings2 + " , will be $"+ finalPrice);
+            console.log(this.pizzaPrice)
+    console.log(finalPrice)
+console.log(this.toppingsPrice )
+    console.log(this.toppingArray)
+    console.log(finalPizza)
     
-    this.pizzaPrice += 35;
-  }
-  return this.pizzaPrice;
-}
 
-
-// Pizza.prototype.toppingPrice = function () {
-  
-//   if (this.toppings === "Mushrooms"){
-//       this.toppingTotal +=1
-//     }
-//     else if (this.toppings === "peperoni"){
-//         this.toppingTotal +=1
-// }
-// else if (this.toppings === "Jalepenos"){
-//   this.toppingTotal +=1
-// }
-//   return this.toppingTotal;
-// }
-
-
-
-
-
-$(document).ready(function() {
-  $("form#pizzaParlor").submit(function(event) {
-    event.preventDefault();``
-    let inputtedSize = $(".form-control#pizzaSlice").val();
-    let inputtedToppings = $("input:radio[name=tops]:checked").val();
-    let inputtedToppings2 = $("input:radio[name=tops2]:checked").val();
-    let finalPizza = new Pizza(inputtedSize, inputtedToppings,inputtedToppings2);
-    
-  $("#order").text("Your , " + " " + inputtedSize + " , "  +"pizza" + " with  , " + inputtedToppings + " , and , " + inputtedToppings2 + " , will be $"+ finalPizza.price());
-    finalPizza.price()
-    
-    console.log(finalPizza);
-    
-  
   });
 });
