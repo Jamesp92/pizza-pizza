f6a7e5720866a804a0ba700e4f44f97a445881e7
pj perry <pjperry28@gmail.com>
2022-06-26 12:53:08 -0700

    WIP still working on topping price

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
  
 // // Bussiness logic for orders
 
-function Pizza(size,toppings) {
+function Pizza(size) {
   this.size = size;
   this.pizzaPrice = 10;
 
@@ -34,26 +34,23 @@ function Pizza(size,toppings) {
               }
               return this.pizzaPrice;
             }
-            
             let inputtedSize = $(".form-control#pizzaSlice").val();
             let inputtedToppings = $("input:radio[name=tops]:checked").val();
             let inputtedToppings2 = $("input:radio[name=tops2]:checked").val();
             let finalPizza = new Pizza(inputtedSize);
-            let finalPrice = this.toppingsPrice + this.pizzaPrice
-
-            this.toppingsPrice = 0;
-            this.toppingArray = [inputtedToppings,inputtedToppings2];
-            this.toppingArray.forEach(() => {
-              this.toppingsPrice += 2;
-              return this.toppingsPrice 
-            });
-            $("#order").text("Your , " + " " + inputtedSize + " , "  +"pizza" + " with  , " + inputtedToppings + " , and , " + inputtedToppings2 + " , will be $"+ finalPrice);
-            console.log(this.pizzaPrice)
-    console.log(finalPrice)
-console.log(this.toppingsPrice )
-    console.log(this.toppingArray)
-    console.log(finalPizza)
-    
-
+            
+        
+              this.toppingArray = [inputtedToppings,inputtedToppings2];
+              this.toppingsPrice = 0;
+              this.toppingArray.forEach(() => {
+                this.toppingsPrice += 2;
+              });
+              console.log(this.toppingsPrice)
+              console.log(this.toppingArray)
+            
+          
+            
+            $("#order").text("Your , " + " " + inputtedSize + " , "  +"pizza" + " with  , " + inputtedToppings + " , and , " + inputtedToppings2 + " , will be $"+ finalPizza.price());
+        
   });
 });
